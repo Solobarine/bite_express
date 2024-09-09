06c0db123134489eac31176a0c39599030476bcb
Solly <solobarine@gmail.com>
2024-09-09 12:42:36 +0100

    Update Review validation and seed data

--- backend/app/models/review.rb
+++ backend/app/models/review.rb
@@ -1,5 +1,5 @@
 class Review < ApplicationRecord
-  validates :title, length: { minimum: 4, maximum: 300 }
+  validates :title, length: { minimum: 1, maximum: 300 }
   validates :content, presence: true, length: { minimum: 2, maximum: 500 }
   validates :ratings, presence: true, numericality: true, comparison: { greater_than: 0, less_than_or_equal_to: 5 }
 
